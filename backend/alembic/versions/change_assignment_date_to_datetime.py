7e6229d195175359f5dce670149a5060168210bd
Izaek256 <izaek256@gmail.com>
2026-08-09 21:43:29 +0300

    chore: update down_revision to merge_all_heads in migration script

--- backend/alembic/versions/change_assignment_date_to_datetime.py
+++ backend/alembic/versions/change_assignment_date_to_datetime.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd1e2f3a4b5c6'
-down_revision: Union[str, Sequence[str], None] = 'c2d3e4f5a6b7'
+down_revision: Union[str, Sequence[str], None] = 'merge_all_heads'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
